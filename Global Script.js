e2e81560fba7b1ff8a3c12f7815ede3d04f79d40
Michael Remediakis <m_remedi@yahoo.com>
2016-07-16 12:41:59 -0700

    Additions to Footer + Sidebar

--- Global Script.js
+++ Global Script.js
@@ -1,12 +1,16 @@
 function openTopics() {
     document.getElementById("topicsContainer").classList.toggle("show");
 }
+function openResources() {
+    document.getElementById("resContainer").classList.toggle("show");
+}
 
 $(document).ready(function () {
-    $("#topicsContainer, #topicsbtn").click(function (e) {
+    $("#topicsContainer, #topicsbtn, #resBtn, #resContainer").click(function (e) {
         e.stopPropagation();
     });
     $(document).click(function(){
         $("#topicsContainer").removeClass("show");
+        $("#resContainer").removeClass("show");
     });
 });
