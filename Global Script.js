f90e5db3bf2cf26b18856718fb9bdd5e2c417315
Michael Remediakis <m_remedi@yahoo.com>
2016-07-18 23:18:27 -0700

    Finally figured out resizable divs
    Yasssss, I finally figured out how to make two resizable divs for the
    website!!!!! In addition, I made a very basic design for the resource
    button because I can't really think of one right now. Small changes to
    the z-indexes of the dropdowns and the shading div when you open the
    sidebar.

--- Global Script.js
+++ Global Script.js
@@ -4,8 +4,23 @@ $(document).ready(function () {
         $("#sideBar").toggle("slide", 250);
         $("#shade").toggle("fade", 250);
     });
-    $("#resources").click(function () {
+    $("#resBtn").click(function () {
         $("#resContainer").toggleClass("show");
+    });
+
+    $("#resizeBar").draggable({
+        axis: "x",
+        containment: "#tutorial",
+        drag: function () {
+            var offset = $(this).offset();
+            var xPos = offset.left;
+            $("#right").css({
+                "left": xPos
+            });
+            $("#left").css({
+                "right": "calc(100vw - " + xPos + "px)"
+            });
+        }
     });
 
 });
@@ -15,13 +30,13 @@ function showTopics() {
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('#topicsBtn, #topicsContainer, #topicsContainer *')) {
+    if (!event.target.matches('#topics, #topics *')) {
         var dropdown = document.getElementById("topicsContainer");
         if (dropdown.classList.contains('show')) {
             dropdown.classList.remove('show');
         }
     }
-    if (!event.target.matches('#resources, #resources *, #resContainer, #resContainer *')) {
+    if (!event.target.matches('#resources, #resources *')) {
         var dropdown2 = document.getElementById("resContainer");
         if (dropdown2.classList.contains('show')) {
             dropdown2.classList.remove('show');
