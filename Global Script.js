53fad0af7c0e463f31f4613ac00506614c3fc278
Michael Remediakis <m_remedi@yahoo.com>
2016-07-18 23:44:16 -0700

    Made some changes to the resizing calculations
    Changed the resizing calculations so that the final result would be a
    percentage of the width of the page rather than just pixels. The problem
    with pixels was that if the user shrunk the browser width to a width
    smaller than the size of the left div's pixel size, the site would try
    to grow to fit the div.

--- Global Script.js
+++ Global Script.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
     $("#resizeBar").draggable({
         axis: "x",
         containment: "#tutorial",
+        refreshPositions: true,
         drag: function () {
             var offset = $(this).offset();
             var xPos = offset.left;
@@ -19,7 +20,22 @@ $(document).ready(function () {
             });
             $("#left").css({
                 "right": "calc(100vw - " + xPos + "px)"
+
             });
+        },
+        stop: function () {
+            var offset = $(this).offset();
+            var xPos = offset.left;
+            $("#right").css({
+                "left": (xPos / $(document).width())*100 + "%"
+            });
+            $("#left").css({
+                "right": (1 - (xPos / $(document).width()))*100 + "%"
+            });
+            $(this).css({
+                "left": (xPos / $(document).width())*100 + "%"
+            })
+
         }
     });
 
